3dd396c5dad2d048027578f0f5412a5837149839
Bharadwaj Talabattula <bharawajtalabattula@gmail.com>
2026-04-12 15:18:17 +0530

    fix health route api update

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // ------ Routes ------------------------------------------
-// app.use('/api', routes);
+ app.use('/api', routes);
 
 
 // Root
